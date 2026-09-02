195e28b042839c2a5b6bc9f1bfc6a76caa860837
iharshkaran <harshkaranofficial@gmail.com>
2026-09-02 21:23:43 +0530

    feat: add custom validation feedback to listing forms

--- public/js/script.js
+++ public/js/script.js
@@ -3,3 +3,21 @@ function toggleMenu() {
     const menu = document.getElementById('navMenu');
     menu.classList.toggle('show');
 }
+
+// custom Bootstrap validation
+(() => {
+    'use strict'
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
